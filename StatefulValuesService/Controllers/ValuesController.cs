d620d74882cebcf06e8c2d375fd8c4e7bb8b77ee
Chris' Mac <chris@MacBook.local>
2016-09-29 00:20:08 -0700

    Added local file state

--- StatefulValuesService/Controllers/ValuesController.cs
+++ StatefulValuesService/Controllers/ValuesController.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace StatefulValuesService.Controllers
@@ -9,36 +8,47 @@ namespace StatefulValuesService.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private const string DATAFILE = @"datafile.csv";
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
+            return ReadValues();
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
+            var values = ReadValues().ToList();
+            values.Add(value);
+            WriteValues(values.ToArray());
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+        private string[] ReadValues()
+        {
+            CheckFile();
+            string data = System.IO.File.ReadAllText(DATAFILE);
+            if (string.IsNullOrEmpty(data)) return new string[0];
+            
+            return data.Split(',');
+        }
+
+        private void WriteValues(string[] values)
+        {
+            System.IO.File.WriteAllText(DATAFILE, string.Join(",", values));
+        }
+
+        private void CheckFile()
+        {
+            if (!(System.IO.File.Exists(DATAFILE))) System.IO.File.Create(DATAFILE).Dispose();
         }
     }
 }
