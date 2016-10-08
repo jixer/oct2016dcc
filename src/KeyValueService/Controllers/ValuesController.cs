0663738d0bfc3941b2d1c63bf26a38e4c79459a2
Chris' Mac <chris@MacBook.local>
2016-10-08 13:52:36 -0700

    added mongo support

--- src/KeyValueService/Controllers/ValuesController.cs
+++ src/KeyValueService/Controllers/ValuesController.cs
@@ -1,6 +1,8 @@
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Bson;
 using System;
 
 namespace DesertCodeCamp.DockerNetCore.KeyValueService.Controllers
@@ -8,20 +10,19 @@ namespace DesertCodeCamp.DockerNetCore.KeyValueService.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        private const string ValuesDataFile = @"_Data/values.dat";
-        private static IDictionary<string, string> _values;
-        private static object _syncLock = new object();
+        private IMongoCollection<BsonDocument> _mongoCollection = new MongoClient("mongodb://db:27017").GetDatabase("default").GetCollection<BsonDocument>("values");
+
 
         // GET api/values
         [HttpGet]
         public IEnumerable<KeyValuePair<string, string>> Get()
         {
             IEnumerable<KeyValuePair<string, string>> retVal;
-            
-            Console.WriteLine("Fetching values from flate file ({0})", ValuesDataFile);
-            retVal = GetValues().ToArray();
-            Console.WriteLine("Successfully fetched values from flate file ({0})", ValuesDataFile);
 
+            Console.WriteLine("Fetching data from Mongo");
+            retVal = Graft(_mongoCollection.Find(x => true).ToList());
+            // implement logging
+             
             return retVal;
         }
 
@@ -29,82 +30,47 @@ namespace DesertCodeCamp.DockerNetCore.KeyValueService.Controllers
         [HttpGet("{id}")]
         public IActionResult Get(string id)
         {
-            var values = GetValues();
-            if (values.ContainsKey(id))
-                return Ok(values[id]);
-             
-             return NotFound();
+            var result = Graft(_mongoCollection.Find(Builders<BsonDocument>.Filter.Eq("_id", id)).ToList()).FirstOrDefault();
+            
+            if (result.Key == null)
+                return NotFound();
+
+            return Ok(result.Value);
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody] KeyValuePair<string, string> v)
         {
-            var values = GetValues();
-            if (values.ContainsKey(v.Key))
-            {
-                values[v.Key] = v.Value;
-                Write(v);
-            }
+            BsonDocument doc = Graft(v);
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", v.Key);
+            if (_mongoCollection.Count(filter) == 0)
+                _mongoCollection.InsertOne(doc);
             else
-            {
-                values.Add(v.Key, v.Value);
-                Write(values);
-            }
+                _mongoCollection.ReplaceOne(filter, doc);         
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(string id)
         {
-            var values = GetValues();
-            
-            if (values.ContainsKey(id)){
-                values.Remove(id);
-            }
-
-            Write(values);
+            _mongoCollection.DeleteOne(Builders<BsonDocument>.Filter.Eq("_id", id));
         }
 
-        private IDictionary<string, string> GetValues()
+        private IEnumerable<KeyValuePair<string, string>> Graft(IList<BsonDocument> bsonDocs)
         {
-            if (_values == null) {
-                lock (_syncLock)
-                {
-                    if (_values == null) {
-                        _values = 
-                            System.IO.File.ReadAllLines(ValuesDataFile)
-                                .Where(x => x != "")
-                                .Select(s => s.Split('\t'))
-                                .ToDictionary(kv => kv[0], kv => kv[1]);
-                    }
-                }
-            }
-
-            return _values;
+            return bsonDocs.ToDictionary(x => (string)x.GetElement("_id").Value, x => (string)x.GetElement("Value").Value).ToArray();
         }
 
-
-
-        private void Write(IDictionary<string, string> values)
+        private BsonDocument Graft(KeyValuePair<string, string> v)
         {
-            lock (_syncLock)
+            var elements = new Dictionary<string, string>
             {
-                System.IO.File.WriteAllText(ValuesDataFile, "");                
-            }
+                {"_id", v.Key},
+                {"Value", v.Value}
+            };
 
-            foreach (KeyValuePair<string, string> value in values)
-            {
-                Write(value);
-            }
-        }
-
-        private void Write(KeyValuePair<string, string> v)
-        {
-            lock (_syncLock)
-            {
-                System.IO.File.AppendAllText(ValuesDataFile, string.Join("\t", new string[] {v.Key, v.Value}) + "\r\n");
-            }
+            return new BsonDocument(elements);
         }
     }
 }
